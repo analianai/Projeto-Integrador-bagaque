d37ed5e88faa0cc6a3d8ff46ee53974c350621d5
Anália <analia.nai@gmail.com>
2023-04-13 20:05:08 -0300

    Users inserido

---
+++ BackendBagaque/BackendBagaque/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace BackendBagaque.Models
+{
+    public class Users
+    {
+        public int IdUsers { get; set; }
+        public string NameUser { get; set; }
+        public string CPFUser { get; set; }
+        public string PhoneUser { get; set; }
+        public string SenhaLoginUser { get; set; }
+        public string EmailLoginUser { get; set; }
+        public int TypeUser { get; set; }
+        public string PostalCodeAddressUser { get; set; }
+        public string NumberAddressUser { get; set; }
+    }
+}
