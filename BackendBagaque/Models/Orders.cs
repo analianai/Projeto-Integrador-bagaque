c634145658d392be84f8ff8682dfbf1cd7df8f57
Anália Emília Souza <82737206+analianai@users.noreply.github.com>
2023-04-14 19:35:12 -0300

    Users e Orders

---
+++ BackendBagaque/Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendBagaque.Models
+{
+    public class Orders
+    {
+        public int IdOrder { get; set; }
+        public int DateOrder { get; set; }
+        public int FinalDateDeliveryOrder { get; set; }
+        public int CodeDeliveryOrder { get; set; }
+        public string StatusOrder { get; set; }
+        public string TypePaymentOrder { get; set; }
+        public string StatusPaymentOrder { get; set; }
+ 
+    }
+}
